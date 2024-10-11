ebd86bef11e6e52c6cc3990c2665c92145b7fa83
andres felipe racines caro <racinescaroa@gmail.com>
2024-10-11 09:00:18 -0500

    se hace entrega de la evaluacion

---
+++ modules/menu.py
@@ -0,0 +1,11 @@
+print ("""
+'''''''''''''''''''''
+Gestor de Tareas CampusTask
+
+'''''''''''''''''''''
+""")
+
+print ('1.Gestión de Tableros')
+print('2.Gestión de Listas')
+print('3.Gestión de Tarjetas')
+
